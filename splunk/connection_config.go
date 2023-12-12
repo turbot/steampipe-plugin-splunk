a2fb3884be48e00659c72e98a75e55b4693b2b30
Ved misra <47312748+misraved@users.noreply.github.com>
2023-12-12 21:07:07 +0530

    Recompile plugin with steampipe-plugin-sdk v5.8.0 (#28)

--- splunk/connection_config.go
+++ splunk/connection_config.go
@@ -2,33 +2,14 @@ package splunk
 
 import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
 )
 
 type splunkConfig struct {
-	URL                *string `cty:"url"`
-	Username           *string `cty:"username"`
-	Password           *string `cty:"password"`
-	AuthToken          *string `cty:"auth_token"`
-	InsecureSkipVerify *bool   `cty:"insecure_skip_verify"`
-}
-
-var ConfigSchema = map[string]*schema.Attribute{
-	"url": {
-		Type: schema.TypeString,
-	},
-	"username": {
-		Type: schema.TypeString,
-	},
-	"password": {
-		Type: schema.TypeString,
-	},
-	"auth_token": {
-		Type: schema.TypeString,
-	},
-	"insecure_skip_verify": {
-		Type: schema.TypeBool,
-	},
+	URL                *string `hcl:"url"`
+	Username           *string `hcl:"username"`
+	Password           *string `hcl:"password"`
+	AuthToken          *string `hcl:"auth_token"`
+	InsecureSkipVerify *bool   `hcl:"insecure_skip_verify"`
 }
 
 func ConfigInstance() interface{} {
